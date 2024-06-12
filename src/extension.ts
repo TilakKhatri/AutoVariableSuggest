bc9c07f5e115ef8634e5b1ca7f5b4a7d6c42dd29
TilakKhatri <tilakkhatri0001@gmail.com>
2024-06-12 23:14:21 +0545

    Feat: remove keyword

--- src/extension.ts
+++ src/extension.ts
@@ -1,82 +1,119 @@
 import * as vscode from "vscode";
 
+const keywords = ["print", "import", "printf", "die"]; // Add your keywords here
+const regex = new RegExp(`(${keywords.join("|")})`, "gi");
+
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand(
     "extension.highlightKeywords",
     async () => {
       const editor = vscode.window.activeTextEditor;
+
       if (!editor) {
         return;
       }
-
-      let ranges = getKeywordRanges(editor);
+      const document = editor.document;
+      let ranges = getKeywordRanges(document, regex);
       editor.setDecorations(decorationType, ranges);
     }
   );
 
   context.subscriptions.push(disposable);
 
-  let disposableRemoveKeyword = vscode.commands.registerCommand(
+  let disposableRemoveKeywords = vscode.commands.registerCommand(
     "extension.removekeywords",
     async () => {
-      vscode.window.showInformationMessage("Hello from removekeywords");
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        return;
+      }
+
+      await removeKeywords(editor);
+      return;
     }
   );
-  context.subscriptions.push(disposableRemoveKeyword);
+  context.subscriptions.push(disposableRemoveKeywords);
 
-  let disposableCommentKeyword = vscode.commands.registerCommand(
+  let disposableCommentKeywords = vscode.commands.registerCommand(
     "extension.commentKeywords",
     async () => {
       const editor = vscode.window.activeTextEditor;
       if (!editor) {
         return;
       }
-      vscode.window.showInformationMessage("Hello from comment keywords");
-      // let ranges: vscode.DecorationOptions[] = getKeywordRanges(editor);
-      const languageID = await getActiveFileLanguage().then(
-        (languageId) => languageId
-      );
-      console.log(languageID);
-      let ranges: any = getKeywordRanges(editor);
-      console.log(ranges);
-      // Iterate over each range and insert a comment around it
-      for (const obj of ranges) {
-        let range = obj.range;
-        const startPos = range.c;
-        const endPos = range.e;
-        // console.log(startPos, endPos);
-
-        await editor.edit((editBuilder) => {
-          editBuilder.insert(startPos, "#");
-          editBuilder.insert(endPos, "\n"); // Insert a newline after the keyword
-        });
-      }
+      await commentOutLinesContainingKeywords(editor);
     }
   );
-  context.subscriptions.push(disposableCommentKeyword);
+  context.subscriptions.push(disposableCommentKeywords);
 }
 let decorationType = vscode.window.createTextEditorDecorationType({
   backgroundColor: "rgba(255,255,0,0.3)", // Yellow background
   border: "1px solid red", // Red border
 });
 
-function getKeywordRanges(editor: any) {
+async function removeKeywords(editor: vscode.TextEditor) {
   const document = editor.document;
-  const keywords = ["print", "import", "printf", "die"]; // Add your keywords here
-  const regex = new RegExp(`(${keywords.join("|")})`, "gi");
-  // console.log("regex", regex);
-  // Find all occurrences of the keywords
-  // const ranges: vscode.DecorationOptions[] = [];
-  const ranges = [];
+  // const keywords = ["print", "import", "printf", "die"];
+  // const regex = new RegExp(`(${keywords.join("|")})`, "gi");
+
   let match;
   while ((match = regex.exec(document.getText())) !== null) {
     const startPos = document.positionAt(match.index);
     const endPos = document.positionAt(match.index + match[0].length);
-    const range = new vscode.Range(startPos, endPos);
-    ranges.push({ range });
+    await editor.edit((editBuilder) =>
+      editBuilder.delete(new vscode.Range(startPos, endPos))
+    );
+  }
+}
+
+async function commentOutLinesContainingKeywords(editor: vscode.TextEditor) {
+  const document = editor.document;
+  // const keywords = ["print", "import", "printf", "die"];
+  // const regex = new RegExp(`(${keywords.join("|")})`, "gi");
+  const ranges = getKeywordRanges(document, regex);
+
+  for (const range of ranges) {
+    const startPos = range.start;
+    const endPos = range.end;
+    const line = document.lineAt(startPos.line);
+    const newText = `#`;
+    await editor.edit((editBuilder) =>
+      editBuilder.replace(new vscode.Range(startPos, startPos), newText)
+    );
+  }
+}
+
+function getKeywordRanges(
+  document: vscode.TextDocument,
+  regex: RegExp
+): vscode.Range[] {
+  const ranges: vscode.Range[] = [];
+  let match;
+  while ((match = regex.exec(document.getText())) !== null) {
+    const startPos = document.positionAt(match.index);
+    const endPos = document.positionAt(match.index + match[0].length);
+    ranges.push(new vscode.Range(startPos, endPos));
   }
   return ranges;
 }
+
+// function getKeywordRanges(editor: any) {
+//   const document = editor.document;
+//   const keywords = ["print", "import", "printf", "die"]; // Add your keywords here
+//   const regex = new RegExp(`(${keywords.join("|")})`, "gi");
+//   // console.log("regex", regex);
+//   // Find all occurrences of the keywords
+//   // const ranges: vscode.DecorationOptions[] = [];
+//   const ranges = [];
+//   let match;
+//   while ((match = regex.exec(document.getText())) !== null) {
+//     const startPos = document.positionAt(match.index);
+//     const endPos = document.positionAt(match.index + match[0].length);
+//     const range = new vscode.Range(startPos, endPos);
+//     ranges.push({ range });
+//   }
+//   return ranges;
+// }
 export function getActiveFileLanguage(): Thenable<string | undefined> {
   return vscode.window.activeTextEditor
     ? Promise.resolve(vscode.window.activeTextEditor.document.languageId)
